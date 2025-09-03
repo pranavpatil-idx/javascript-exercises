3a11b208433d1c5af1440258ef4be4753e5f7158
pranavpatil-idx <pranav.patil@indexnine.com>
2025-09-03 19:57:27 +0530

    12_calculator

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a,b) {
+  const ans = a+b;
+  return ans;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+  const ans = a - b;
+  return ans;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce( (total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce( (total, current) => total * current)
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+  return Math.pow(a,b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let ans = 1;
+  for(let i = num; i>0; i--){
+    ans *= i;
+  }
+  return ans; 
 };
 
 // Do not edit below this line
