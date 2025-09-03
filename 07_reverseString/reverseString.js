a0fb0907a98d483df0098c16ec1a9460bafeb784
pranavpatil-idx <pranav.patil@indexnine.com>
2025-09-03 14:37:39 +0530

    07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(input) {
+    const ans = input.split(''); 
+    const rev = ans.reverse();
+    const final = rev.join('');
+    return final;
 };
 
 // Do not edit below this line
