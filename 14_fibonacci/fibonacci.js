af7822f439ac1893c011ae40bbb484b7e2194222
pranavpatil-idx <pranav.patil@indexnine.com>
2025-09-04 11:22:04 +0530

    14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(numArg) {
 
+    let num;
+    if(typeof numArg !== 'number'){
+        num = parseInt(numArg)
+    } else {
+        num = numArg;
+    }
+
+    if(num === 0) return 0;
+
+    if(num<0) return "OOPS";
+
+    let a = 0;
+    let b = 1;
+    for(let i = 2; i<= num; i++){
+        let next = a + b;
+        a = b;
+        b = next;
+    }
+    return b;
 };
 
 // Do not edit below this line
